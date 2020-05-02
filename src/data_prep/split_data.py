a29d210902ed78ab8fb750ef60ee0867a108ea2a
Minyus <yusuke.minami86@gmail.com>
2020-05-02 12:07:32 +0000

    Expose test_size param

--- src/data_prep/split_data.py
+++ src/data_prep/split_data.py
@@ -3,6 +3,6 @@ from sklearn.model_selection import train_test_split
 
 def split_data(df, test_size=0.2, seed=None):
     train_df, test_df = train_test_split(
-        df, test_size=0.2, random_state=seed, stratify=df["Treatment"]
+        df, test_size=test_size, random_state=seed, stratify=df["Treatment"]
     )
     return train_df, test_df
