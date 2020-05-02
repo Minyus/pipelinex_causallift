8252f1a80b7b800a3729df7fd9a4c1d86763e53f
Minyus <yusuke.minami86@gmail.com>
2020-05-02 11:28:51 +0000

    Separate data_prep params from causallift_params

--- src/data_prep/sim_data.py
+++ src/data_prep/sim_data.py
@@ -1,18 +1,16 @@
-def get_sim_data(parameters):
+def get_sim_data(seed):
     """
     # Generate simulated data
-    # "Sleeping dogs" (a.k.a. "do-not-disturb"; people who will "buy" if not 
-    treated but will not "buy" if treated) can be simulated by negative values 
+    # "Sleeping dogs" (a.k.a. "do-not-disturb"; people who will "buy" if not
+    treated but will not "buy" if treated) can be simulated by negative values
     in tau parameter.
-    # Observational data which includes confounding can be simulated by 
-    non-zero values in propensity_coef parameter.  
-    # A/B Test (RCT) with a 50:50 split can be simulated by all-zeros values 
+    # Observational data which includes confounding can be simulated by
+    non-zero values in propensity_coef parameter.
+    # A/B Test (RCT) with a 50:50 split can be simulated by all-zeros values
     in propensity_coef parameter (default).
     # The first element in each list parameter specifies the intercept.
     """
     from causallift import generate_data
-
-    seed = parameters.get("seed")
 
     df = generate_data(
         N=1000,
--- src/data_prep/split_data.py
+++ src/data_prep/split_data.py
@@ -1,8 +1,7 @@
 from sklearn.model_selection import train_test_split
 
 
-def split_data(parameters, df):
-    seed = parameters.get("seed")
+def split_data(df, test_size=0.2, seed=None):
     train_df, test_df = train_test_split(
         df, test_size=0.2, random_state=seed, stratify=df["Treatment"]
     )
